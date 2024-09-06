8d216006ecb465771e0b538bc1cb176b9063ab73
nahalasif <nahalumer1122@gmail.com>
2024-09-06 23:47:09 +0300

    done

---
+++ src/OrganizationComponent.java
@@ -0,0 +1,11 @@
+
+import java.util.List;
+
+public interface OrganizationComponent {
+    String getName();
+    double getSalary();
+    void add(OrganizationComponent organizationComponent);
+    void remove(OrganizationComponent organizationComponent);
+    List<OrganizationComponent> getChildren();
+    String getDetails();
+}
